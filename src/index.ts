b3556fb61fb3f075798a46f2f24c9d4a43419795
Shivaji Chalise <respond2shivaji@gmail.com>
2024-02-09 11:32:55 +0545

    making the counter async

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { ethers } from "ethers"
+import Counter from "../artifacts/contracts/Counter.sol/Counter.json"
+
 function getEth() {
   // @ts-ignore
   const eth = window.ethereum
@@ -35,27 +37,26 @@ async function run() {
   const provider = new ethers.BrowserProvider(getEth())
 
   const counter = new ethers.Contract(
-    "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-    [
-      "function count() public",
-      "function getCounter() public view returns (uint32)",
-    ],
+    "0x9a676e781a523b5d0c0e43731313a708cb607508",
+    Counter.abi,
     await provider.getSigner()
   )
 
   const el = document.createElement("div")
-  async function setCounter() {
-    el.innerHTML = await counter.getCounter()
+  async function setCounter(count?) {
+    el.innerHTML = count || (await counter.getCounter())
   }
   setCounter()
 
   const button = document.createElement("button")
   button.innerText = "increment"
   button.onclick = async function () {
-    const tx = await counter.count()
-    await tx.wait()
-    setCounter()
+    await counter.count()
   }
+
+  counter.on(counter.filters.CounterInc(), function (count) {
+    setCounter(count.count)
+  })
 
   document.body.appendChild(el)
   document.body.appendChild(button)
